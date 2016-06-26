9291b0336872feaf6bcd7e36ece148c6a46cbbba
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-06-26 07:42:39 -0500

    Create data transfer objects.

--- BookService/Controllers/BooksController.cs
+++ BookService/Controllers/BooksController.cs
@@ -18,17 +18,37 @@ namespace BookService.Controllers
         private BookServiceContext db = new BookServiceContext();
 
         // GET: api/Books
-        public IQueryable<Book> GetBooks()
+        public IQueryable<BookDto> GetBooks()
         {
-            return db.Books
-                .Include(b => b.Author);
+            var books = from b in db.Books
+                select new BookDto()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    AuthorName = b.Author.Name
+                };
+            return books;
+
         }
 
         // GET: api/Books/5
-        [ResponseType(typeof(Book))]
+        [ResponseType(typeof(BookDetailDto))]
         public async Task<IHttpActionResult> GetBook(int id)
         {
-            Book book = await db.Books.FindAsync(id);
+            var book = await db.Books.Include(b => b.Author)
+                .Select(b => new BookDetailDto()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Year = b.Year,
+                    Price = b.Price,
+                    AuthorName = b.Author.Name,
+                    Genre = b.Genre
+                })
+                .SingleOrDefaultAsync(b => b.Id == id);
+        
+
+          
             if (book == null)
             {
                 return NotFound();
@@ -84,7 +104,14 @@ namespace BookService.Controllers
             db.Books.Add(book);
             await db.SaveChangesAsync();
 
-            return CreatedAtRoute("DefaultApi", new { id = book.Id }, book);
+            var dto = new BookDto()
+            {
+                Id = book.Id,
+                Title = book.Title,
+                AuthorName = book.Author.Name
+            };
+
+            return CreatedAtRoute("DefaultApi", new { id = book.Id }, dto);
         }
 
         // DELETE: api/Books/5
